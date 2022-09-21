1865016fdcef8f1a207589e444d4846d7260f0ca
Martín <magadiflo@gmail.com>
2022-09-20 19:57:31 -0500

    Creando nuestra interfaz Feign Client

---
+++ src/main/java/com/magadiflo/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,15 @@
+package com.magadiflo.oauth.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.magadiflo.commons.usuarios.models.entity.Usuario;
+
+@FeignClient(name = "servicio-usuarios")
+public interface UsuarioFeignClient {
+
+	@GetMapping("/usuarios/search/buscar-username")
+	public Usuario findByUsername(@RequestParam String username);
+
+}
