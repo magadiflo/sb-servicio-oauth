a99abc0f95e2388ad913a84c5bbdbb12e77d2b96
Martín <magadiflo@gmail.com>
2022-09-21 18:12:42 -0500

    Probando la autenticación con Postman y obteniendo el token JWT

--- src/main/java/com/magadiflo/oauth/SbServicioOauthApplication.java
+++ src/main/java/com/magadiflo/oauth/SbServicioOauthApplication.java
@@ -1,17 +1,34 @@
 package com.magadiflo.oauth;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class SbServicioOauthApplication {
+public class SbServicioOauthApplication implements CommandLineRunner {
+	
+	private final PasswordEncoder passwordEncoder;
+	
+	public SbServicioOauthApplication(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;		
+	}	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbServicioOauthApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String password = "12345";
+		for(int i = 0; i < 4; i++) {
+			String passwordBcrypt = this.passwordEncoder.encode(password);
+			System.out.println(passwordBcrypt);
+		}
 	}
 
 }
