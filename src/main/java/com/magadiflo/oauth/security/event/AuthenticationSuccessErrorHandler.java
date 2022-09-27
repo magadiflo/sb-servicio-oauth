c3a9c804fe6c2850f107241b64bd004ad1a6fc5a
Martín <magadiflo@gmail.com>
2022-09-26 19:04:20 -0500

    Implementando evento tres intentos en el login - parte 2

--- src/main/java/com/magadiflo/oauth/security/event/AuthenticationSuccessErrorHandler.java
+++ src/main/java/com/magadiflo/oauth/security/event/AuthenticationSuccessErrorHandler.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import com.magadiflo.commons.usuarios.models.entity.Usuario;
 import com.magadiflo.oauth.services.IUsuarioService;
 
 @Component // Está siendo inyectada en el SpringSecurityConfig (vía constructor y usando la interfaz AuthenticationEventPublisher)
@@ -31,12 +32,40 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 		System.out.printf("Success login: %s%n", userDetails.getUsername());
 		LOG.info("Success login: {}", userDetails.getUsername());
+		
+		Usuario usuario = this.usuarioService.findByUsername(authentication.getName());
+		if(usuario.getIntentos() != null && usuario.getIntentos() > 0) {
+			usuario.setIntentos(0);
+			this.usuarioService.update(usuario, usuario.getId());
+		}
 	}
 
 	@Override
 	public void publishAuthenticationFailure(AuthenticationException exception, Authentication authentication) {
 		System.out.printf("Error en el login: %s%n", exception.getMessage());
 		LOG.error("Error en el login: {}", exception.getMessage());
+		
+		try {
+			Usuario usuario = this.usuarioService.findByUsername(authentication.getName());
+			if(usuario.getIntentos() == null) {
+				usuario.setIntentos(0);
+			}
+			
+			LOG.info("Valor actual de 'intentos': {}", usuario.getIntentos());
+			usuario.setIntentos(usuario.getIntentos() + 1);
+			LOG.info("Valor después de actualizar 'intentos': {}", usuario.getIntentos());
+			
+			if(usuario.getIntentos() >= 3) {
+				LOG.error("El usuario {} deshabilitado por máximo de intentos", authentication.getName());
+				usuario.setEnabled(false);//Deshabilitamos al usuario				
+			}
+			
+			//Actualizamos el usuario en la BD
+			this.usuarioService.update(usuario, usuario.getId());
+		}catch (Exception e) {
+			LOG.error("El usuario {} no existe en el sistema", authentication.getName());
+		}
+		
 	}
 
 }
